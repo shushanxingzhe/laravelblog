2b7bce9d30a1596e87e951b53ac71fa9e7e11643
root <root@localhost.localdomain>
2014-11-28 01:01:50 -0800

    first commit

---
+++ app/database/seeds/SentrySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+class SentrySeeder extends Seeder {
+
+  public function run()
+    {
+        DB::table('users')->delete();
+        DB::table('groups')->delete();
+        DB::table('users_groups')->delete();
+
+    Sentry::getUserProvider()->create(array(
+          'email'      => 'oo@xx.com',
+          'password'   => "ooxx",
+          'first_name' => 'OO',
+          'last_name'  => 'XX',
+          'activated'  => 1,
+      ));
+
+          Sentry::getGroupProvider()->create(array(
+              'name'        => 'Admin',
+	      'permissions' => ['admin' => 1],
+          ));
+	     // ½«Ó»§¼Óë»§×
+	              $adminUser  = Sentry::getUserProvider()->findByLogin('oo@xx.com');
+  		      $adminGroup = Sentry::getGroupProvider()->findByName('Admin');
+	              $adminUser->addGroup($adminGroup);
+    }
+}
